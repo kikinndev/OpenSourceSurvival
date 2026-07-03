fbfa0e2cf3f8bc3d95bffc7197a78f4507465d7c
kikinn <kikinn@DESKTOP-S2JJ85T>
2026-07-03 16:15:12 +0200

    Added a Player Class

---
+++ Entities/Player.cs
@@ -0,0 +1,65 @@
+﻿using Raylib_cs;
+using System.Data;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Main;
+
+public class Player
+{
+    float speed = 200;
+
+    Vector2 position;
+
+    Sprite playerSprite;
+    Sprite handsSprite;
+
+    public Player(Vector2 position)
+    {
+        this.position = position;
+        playerSprite = new Sprite("Resources/player_up.png", position, 3);
+        handsSprite = new Sprite("Resources/player_hands.png", position, 3);
+    }
+
+    public void Update(Vector2 mousePos, float delta)
+    {
+        Vector2 direction = Vector2.Zero;
+
+        if (Raylib.IsKeyDown(KeyboardKey.W))
+        {
+            direction.Y -= 1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.S))
+        {
+            direction.Y += 1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.A))
+        {
+            direction.X -= 1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.D))
+        {
+            direction.X += 1;
+        }
+
+        position += direction * speed * delta;
+        
+        playerSprite.position = position;
+        handsSprite.position = position;
+
+        playerSprite.LookAt(mousePos, 100, delta);
+        handsSprite.LookAt(mousePos, 10, delta);
+    }
+
+    public void Draw()
+    {
+        playerSprite.Draw();
+        handsSprite.Draw();
+    }
+
+    public void Unload()
+    {
+        playerSprite.Unload();
+        handsSprite.Unload();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,58 +14,24 @@ internal static class Program
     {
         Raylib.InitWindow(screenWidth, screenHeight, "Hello World");
 
-        float scale = 3;
-
-        Sprite playerSprite = new Sprite("Resources/player_up.png", new Vector2(screenWidth / 2, screenHeight / 2), scale);
-        Sprite playerHands = new Sprite("Resources/player_hands.png", new Vector2(screenWidth / 2, screenHeight / 2), scale);
-
-        float speed = 200;
+        Player player = new Player(new Vector2(screenWidth / 2, screenHeight / 2));
 
         while (!Raylib.WindowShouldClose())
         {
             float delta = Raylib.GetFrameTime();
-            Vector2 direction = Vector2.Zero;
             Vector2 mousePos = Raylib.GetMousePosition();
 
-            if (Raylib.IsKeyDown(KeyboardKey.W))
-            {
-                direction.Y -= 1;
-            }
-            if (Raylib.IsKeyDown(KeyboardKey.S))
-            {
-                direction.Y += 1;
-            }
-            if (Raylib.IsKeyDown(KeyboardKey.A))
-            {
-                direction.X -= 1;
-            }
-            if (Raylib.IsKeyDown(KeyboardKey.D))
-            {
-                direction.X += 1;
-            }
-
-            if (direction.LengthSquared() > 0)
-            {
-                direction = Vector2.Normalize(direction);
-            }
-
-            playerSprite.position += direction * speed * delta;
-            playerHands.position = playerSprite.position;
-
-            playerSprite.LookAt(mousePos, 100, delta);
-            playerHands.LookAt(mousePos, 10, delta);
+            player.Update(mousePos, delta);
 
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
-            playerSprite.Draw();
-            playerHands.Draw();
+            player.Draw();
 
             Raylib.EndDrawing();
         }
-
-        playerSprite.Unload();
-        playerHands.Unload();
+        
+        player.Unload();
 
         Raylib.CloseWindow();
     }
